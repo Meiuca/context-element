a78d9a404ff20e3ed9ad1cc3cecd6b0b42197a2f
Levi Pires <waltvy.comercial@gmail.com>
2021-04-01 12:49:30 -0300

    chore: :wrench: update and add missing config files

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-unresolved */
 import { playwrightLauncher } from '@web/test-runner-playwright';
 
 export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
