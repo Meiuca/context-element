1bc15e779dd70895780522267289457d248e5589
Levi Pires <waltvy.comercial@gmail.com>
2021-04-05 14:05:57 -0300

    chore(commitlint): :wrench: create scope enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,36 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const glob = require('glob');
+const { promisify } = require('util');
+const path = require('path');
+
+const globPromise = promisify(glob);
+
+async function getFiles(pattern) {
+  const globResult = await globPromise(pattern);
+
+  return globResult.map(item => path.basename(item));
+}
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+
+  rules: {
+    'scope-enum': async () => [
+      2,
+      'always',
+      [
+        'eslint',
+        'prettier',
+        'babel',
+        'commitlint',
+        'snowpack',
+        'web-test-runner',
+        'husky',
+        'package-json',
+        'vscode',
+        // 'proposal',
+        ...(await getFiles('./src/**/*.js')),
+        ...(await getFiles('./test/**/*.js')),
+      ],
+    ],
+  },
+};
