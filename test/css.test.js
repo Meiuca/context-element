c52830719b80799dfb5edbc9beb9aa81866b625c
Levi Pires <waltvy.comercial@gmail.com>
2021-04-14 17:00:05 -0300

    test(test#css.test.js): update test

--- test/css.test.js
+++ test/css.test.js
@@ -15,18 +15,28 @@ describe('css.js', () => {
   });
 
   it('createGooberGetter function should throw an error if props[0] is not an object', () => {
-    expect(createGooberGetter).to.throw("typeof props[0] is 'undefined'; must be 'object'");
+    expect(() => createGooberGetter`${undefined}`).to.throw(
+      "typeof props[0] is 'undefined'; must be 'object'",
+    );
   });
 
   it('createGooberGetter function should return a GooberGetter', () => {
-    const cssReturn = createGooberGetter`${{}}`();
+    const cssReturn = createGooberGetter`${{}}`;
 
-    expect(cssReturn).to.have.property('id').that.is.a('string');
+    expect(cssReturn()).to.have.property('id').that.is.a('string');
 
-    expect(cssReturn)
+    expect(cssReturn())
       .to.have.property('result')
       .that.is.an('object')
       .that.have.property('cssText')
       .that.is.a('string');
+
+    expect(cssReturn.reactify).to.be.a('function');
+
+    expect(cssReturn.reactify('test')).to.be.a('string');
+
+    const globalStyles = document.querySelector('#__themed-element');
+
+    expect(globalStyles).to.be.an.instanceof(HTMLElement, 'the global styles is not being created');
   });
 });
