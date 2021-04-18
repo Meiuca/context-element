106249bdb343ffd5c2b06bca832f828bce9f1c0e
Levi Pires <waltvy.comercial@gmail.com>
2021-04-17 21:12:09 -0300

    feat(src#css.js): add createLitGetter
    along with refactorings

--- src/css.d.ts
+++ src/css.d.ts
@@ -1,41 +1,94 @@
 import { CSSAttribute } from 'goober';
 import { CSSResult } from 'lit-element';
-import { GooberGetter } from './context-element';
 
-declare interface GooberInstance {
+declare interface StyleInstance {
   id: string;
   result: CSSResult;
 }
 
-declare type ThemePropGetter<Theme> = (theme: Theme) => string | number;
+declare interface StyleGetter {
+  (contextId: string): StyleInstance;
 
-declare type CreateGooberGetterProps<Theme> = Array<ThemePropGetter<Theme> | string | number> & {
-  0: Theme;
+  /**
+   * Pushes the CSSResult to the DOM
+   *
+   * @returns If created by `createGooberGetter` returns the goober automagically generated class, else empty string
+   */
+  extract: (contextId: string) => string;
+
+  /**
+   * @deprecated Change to `extract`
+   */
+  reactify: (contextId: string) => string;
+}
+
+declare type ContextPropGetter<Context> = (context: Context) => string | number;
+
+declare type CreateStyleGetterProps<Context> = Array<
+  ContextPropGetter<Context> | string | number
+> & {
+  0: Context;
 };
 
+/**
+ * Encapsulates [Goober](https://github.com/cristianbote/goober)
+ * and LitElement together
+ */
 export function css(
   tag: string | CSSAttribute | TemplateStringsArray,
   ...props: Array<string | number>
-): GooberInstance;
+): StyleInstance;
 
 /**
- * Example:
+ * Creates a dinamic style getter using [goober](https://github.com/cristianbote/goober)
+ *
+ * This function supports Scss syntax
  *
  * ```js
- * import defaultButtonTheme from './my-default-button-theme';
+ * import defaultButtonContext from './my-default-button-context.js';
  * import { createGooberGetter as css } from '@meiuca/context-element';
  *
  * export css`
- * ${defaultButtonTheme}
+ * ${defaultButtonContext}
  *
- * :host {
+ * input {
  *  background-color: ${({ backgroundColor }) => backgroundColor};
- *  color: ${'#1100ff'};
+ *
+ *  &::placeholder {
+ *    color: ${({ color }) => color};
+ *  }
  * }
  * `
  * ```
  */
-export function createGooberGetter<DefaultTheme>(
+export function createGooberGetter<DefaultContext>(
   tag: TemplateStringsArray,
-  ...props: CreateGooberGetterProps<DefaultTheme>
-): GooberGetter;
+  ...props: CreateStyleGetterProps<DefaultContext>
+): StyleGetter;
+
+/**
+ * Creates a dinamic style getter using LitElement `unsafeCSS`.
+ *
+ * This function does *not* support Scss syntax
+ *
+ * ```js
+ * import defaultButtonContext from './my-default-button-context.js';
+ * import { createLitGetter as css } from '@meiuca/context-element';
+ *
+ * export css`
+ * ${defaultButtonContext}
+ *
+ * input {
+ *  background-color: ${({ backgroundColor }) => backgroundColor};
+ * }
+ *
+ * input::placeholder {
+ *  color: ${({ color }) => color};
+ * }
+ * `
+ * ```
+ */
+export function createLitGetter<DefaultContext>(
+  tag: TemplateStringsArray,
+  ...props: CreateStyleGetterProps<DefaultContext>
+): StyleGetter;
--- src/css.js
+++ src/css.js
@@ -1,64 +1,105 @@
-import { css as goCSS, extractCss } from 'goober';
+import { css as gooberCSS, extractCss } from 'goober';
 import { unsafeCSS } from 'lit-element';
 import { getComponentContext } from './context.js';
 
-const wrapper = document.createElement('div');
-const _css = goCSS.bind({ target: wrapper });
+const _wrapper = document.createElement('div');
+const _bindedGooberCSS = gooberCSS.bind({ target: _wrapper });
 
-export function css(...params) {
-  return { id: _css(...params), result: unsafeCSS(extractCss(wrapper)) };
+function css(...params) {
+  const id = _bindedGooberCSS(...params);
+  const result = unsafeCSS(extractCss(_wrapper));
+
+  return { id, result };
 }
 
-export function createGooberGetter(tag, ...props) {
+function _litCss(value) {
+  return { id: '', result: unsafeCSS(value) };
+}
+
+function _extract(componentContextId) {
+  const { bindedGetter } = this;
+
+  let externalStylesTag = document.head.querySelector('#__context-element');
+
+  if (!externalStylesTag) {
+    const externalStyles = document.createElement('style');
+
+    externalStyles.id = '__context-element';
+    externalStyles.innerHTML = '';
+
+    document.head.appendChild(externalStyles);
+
+    externalStylesTag = externalStyles;
+  }
+
+  const {
+    id,
+    result: { cssText },
+  } = bindedGetter(componentContextId);
+
+  const { innerHTML } = externalStylesTag;
+
+  if ((id && !innerHTML.includes(id)) || !innerHTML.includes(cssText))
+    externalStylesTag.innerHTML += `\n${cssText}\n`;
+
+  return id;
+}
+
+function _getter(tag, props, cssFn, componentContextId) {
+  const context = getComponentContext(componentContextId, props[0]);
+
+  const transformedProps = props.map(prop => {
+    if (typeof prop === 'function') {
+      return prop(context);
+    }
+
+    if (typeof prop === 'string' || typeof prop === 'number') {
+      return prop;
+    }
+
+    return '';
+  });
+
+  const interpolate = (prev, curr, idx) => prev + curr + tag[idx + 1];
+
+  const cssText = transformedProps.reduce(interpolate, tag[0]);
+
+  return cssFn(cssText);
+}
+
+function _createGetterObj(getter, tag, props) {
   if (typeof props[0] !== 'object' && props.length > 0) {
     throw new Error(`typeof props[0] is '${typeof props[0]}'; must be 'object'`);
   }
 
-  const gooberGetter = componentContextId => {
-    const context = getComponentContext(componentContextId, props[0]);
+  const bindedGetter = getter.bind({ tag, props });
 
-    const transformedProps = props.map(prop => {
-      if (typeof prop === 'function') {
-        return prop(context);
-      }
+  bindedGetter.extract = _extract.bind({ bindedGetter });
 
-      if (typeof prop === 'string' || typeof prop === 'number') {
-        return prop;
-      }
+  // DEPRECATED
+  bindedGetter.reactify = bindedGetter.extract;
 
-      return '';
-    });
-
-    const cssText = transformedProps.reduce(
-      (prev, curr, idx) => prev + curr + tag[idx + 1],
-      tag[0],
-    );
-
-    return css(cssText);
-  };
-
-  const reactify = componentContextId => {
-    let externalStylesTag = document.head.querySelector('#__context-element');
-
-    if (!externalStylesTag) {
-      const externalStyles = document.createElement('style');
-
-      externalStyles.id = '__context-element';
-      externalStyles.innerHTML = '';
-
-      document.head.appendChild(externalStyles);
-
-      externalStylesTag = externalStyles;
-    }
-
-    const { id, result } = gooberGetter(componentContextId);
-
-    externalStylesTag.innerHTML += `${result.cssText}\n\n\n`;
-
-    return id;
-  };
-
-  gooberGetter.reactify = reactify;
-
-  return gooberGetter;
+  return bindedGetter;
 }
+
+function _gooberGetter(componentContextId) {
+  const { tag, props } = this;
+
+  return _getter(tag, props, css, componentContextId);
+}
+
+function _litGetter(componentContextId) {
+  const { tag, props } = this;
+
+  return _getter(tag, props, _litCss, componentContextId);
+}
+
+function createGooberGetter(tag, ...props) {
+  return _createGetterObj(_gooberGetter, tag, props);
+}
+
+function createLitGetter(tag, ...props) {
+  return _createGetterObj(_litGetter, tag, props);
+}
+
+export { css, createGooberGetter, createLitGetter };
