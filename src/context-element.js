41501901f652556775e94a9da526e21f10546e11
Levi Pires <waltvy.comercial@gmail.com>
2021-04-20 23:45:30 -0300

    fix(src#context-element.js): remove 'async' from updateStyles

--- src/context-element.js
+++ src/context-element.js
@@ -78,7 +78,7 @@ export default function contextElementMixin(getter = []) {
       }
     }
 
-    async updateStyles() {
+    updateStyles() {
       this.allowTransitions = false;
 
       setTimeout(() => {
