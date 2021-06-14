ea50db7a3c59ae131c28545dbc43a445c55cc949
Levi Pires <waltvy.comercial@gmail.com>
2021-06-14 13:38:24 -0300

    feat: add lit-directives.js

---
+++ src/lit-directives.js
@@ -0,0 +1,29 @@
+import * as asyncAppend from 'lit/directives/async-append';
+import * as asyncReplace from 'lit/directives/async-replace';
+import * as cache from 'lit/directives/cache';
+import * as classMap from 'lit/directives/class-map';
+import * as guard from 'lit/directives/guard';
+import * as live from 'lit/directives/live';
+import * as ref from 'lit/directives/ref';
+import * as repeat from 'lit/directives/repeat';
+import * as styleMap from 'lit/directives/style-map';
+import * as templateContent from 'lit/directives/template-content';
+import * as unsafeHtml from 'lit/directives/unsafe-html';
+import * as unsafeSvg from 'lit/directives/unsafe-svg';
+import * as until from 'lit/directives/until';
+
+export const directives = {
+  asyncAppend,
+  asyncReplace,
+  cache,
+  classMap,
+  guard,
+  live,
+  ref,
+  repeat,
+  styleMap,
+  templateContent,
+  unsafeHtml,
+  unsafeSvg,
+  until,
+};
